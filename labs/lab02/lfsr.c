04a1e4be90df7a83cf3cb00683f115e0d159dd0d
Delerrr <2338787978@qq.com>
2022-11-10 20:53:40 +0800

    finished lab02

--- labs/lab02/lfsr.c
+++ labs/lab02/lfsr.c
@@ -5,6 +5,7 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    uint16_t a1 = 1&(*reg), a2 = 1&(*reg>>2), a3 = 1&(*reg>>3), a4 = 1&(*reg>>5);
+    *reg = (*reg>>1 & ~(1<<15)) | ((a1 ^ a2 ^ a3 ^ a4) << 15);
 }
 
